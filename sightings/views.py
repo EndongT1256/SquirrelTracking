0869d5a8d0952fb496fb9c4a454233f2f566add6
Endong Teng <et2697@columbia.edu>
2021-04-14 12:39:56 +0000

    Update views.py

--- sightings/views.py
+++ sightings/views.py
@@ -6,7 +6,10 @@ from .models import Squirrel
 from .forms import AddRequestForm
 from .forms import UpdateRequestForm
 
-
+def index(request):
+    squirrels = Squirrel.objects.all()
+    context = {'squirrels': squirrels}
+    return render(request, 'sightings/home.html', context)
 
 
 def map(request):
